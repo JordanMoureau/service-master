f2b371af36b48759567b65e3ca849f521c32e3f6
Jordan Moureau <jordanfromnovember@gmail.com>
2025-09-19 09:45:39 -0700

    re-direct override

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.smcfireandwater.com" }],
+        destination: "https://smcfireandwater.com/:path*",
+        permanent: true, // 308
+      },
+    ];
+  },
+};
 
 export default nextConfig;
